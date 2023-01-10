18b5dbfce6b3e2fb595d27122ed7ea9e9146889e
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-01-11 00:53:26 +0100

    show shiny when space pressed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const poke_container = document.getElementById('poke_container');
+const poke_container1 = document.getElementById('poke_container1');
 const pokemons_number = 150; // fixe le nombre de pokemon maximun a afficher
 // permet de faire une itération pour que les pokemon soit afficher de 1 jusqu'au nombre choisi
 const fetchPokemons = async () => {
@@ -12,6 +13,7 @@ const getPokemon = async id => { // async sert faire charger plusieur chose en m
   const res = await fetch(url);
   const pokemon = await res.json();
   createPokemonCard(pokemon); // apelle la fonction qui va crée les différante cart pokemon 
+  createPokemonCardShy(pokemon);
   
 
 }
@@ -27,9 +29,9 @@ const createPokemonCard = (pokemon) => {
   const type = types[0].type.name;
   // cretion d'un élement html qui va etre modifier et génerer par le js 
   const pokeInnerHTML = ` 
-  <div class="img-container">
-    <img id="j" src="${sprites. front_default}" alt="${name}"/>
-    <img id="shy" src="${sprites. front_shiny}" alt="${name}2"/>
+    <div id="pok">
+    <div class="img-container">
+    <img src="${sprites. front_default}" alt="${name}"/>
   </div
   <div class="info">
     <span class="number">
@@ -42,11 +44,65 @@ const createPokemonCard = (pokemon) => {
     Type: <span>${type}</span>
     </small>
     </div>
+    </div>
   `
 
   pokemonEL.innerHTML = pokeInnerHTML;
   poke_container.appendChild(pokemonEL);
 }
 
+const createPokemonCardShy = (pokemon) => {
+  const pokemonEL = document.createElement('div'); // creation de div cette div va etre notre card 
+  pokemonEL.classList.add('pokemon');
+  const { id, name, sprites, types } = pokemon;  // on defini la constante pokemon
+  const type = types[0].type.name;
+  // cretion d'un élement html qui va etre modifier et génerer par le js 
+  const pokeInnerHTML = ` 
+    <div id="pok">
+    <div class="img-container">
+    <img src="${sprites. front_shiny}" alt="${name}"/>
+  </div
+  <div class="info">
+    <span class="number">
+    ${id}
+    </span>
+    <h3 class="name">
+    ${name}
+    </h3>
+    <small class="type">
+    Type: <span>${type}</span>
+    </small>
+    </div>
+    </div>
+  `
+
+  pokemonEL.innerHTML = pokeInnerHTML;
+  poke_container1.appendChild(pokemonEL);
+}
+
 fetchPokemons();
+
+function dark(){
+  var element = document.getElementById("poke_container");
+  element.remove();
+
+  
+}
+
+function white(){
+  var element = document.getElementById("poke_container1");
+  
+  element.classList.remove("shy");
+  
+}
+
+document.addEventListener('keyup', event => {
+  if (event.code === 'Space') {
+    
+    dark();
+    white();
+  
+  }
+})
+
 
